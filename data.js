a67d34c8da632beab8836e93fc2f137c8673d489
JesusFernando <yesper233@gmail.com>
2025-07-26 11:46:30 -0600

    Lanzamineto

---
+++ data.js
@@ -0,0 +1,783 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-entrada",
+      "name": "1 Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05037673128783915,
+          "pitch": 0.11208705970929245,
+          "rotation": 0,
+          "target": "1-2-resepcin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2-resepcin",
+      "name": "2 Resepción",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5198846504756673,
+          "pitch": -0.0923994951017626,
+          "rotation": 4.71238898038469,
+          "target": "2-31-entrada-hombres"
+        },
+        {
+          "yaw": 0.6205696274251054,
+          "pitch": -0.06985852085450972,
+          "rotation": 7.853981633974483,
+          "target": "9-41-entrada-mujeres"
+        },
+        {
+          "yaw": 2.9805106274251054,
+          "pitch": 0.16985852085450972,
+          "rotation": 6.853981633974483,
+          "target": "0-1-entrada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-31-entrada-hombres",
+      "name": "3.1 Entrada Hombres",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9080744418208484,
+          "pitch": 0.1075453318073869,
+          "rotation": 0,
+          "target": "3-32-espejos-hombres"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-32-espejos-hombres",
+      "name": "3.2 Espejos Hombres",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.46064638571836625,
+          "pitch": 0.07698875984563891,
+          "rotation": 5.497787143782138,
+          "target": "7-36-duchas-hombres"
+        },
+        {
+          "yaw": 0.6254389736530204,
+          "pitch": -0.01931672173159349,
+          "rotation": 0,
+          "target": "1-2-resepcin"
+        },
+        {
+          "yaw": -1.333743770016234,
+          "pitch": 0.0625986188452039,
+          "rotation": 0.7853981633974483,
+          "target": "6-35-baos-hombres"
+        },
+        {
+          "yaw": -1.8635812827058,
+          "pitch": 0.0799338467505688,
+          "rotation": 10.995574287564278,
+          "target": "4-33-lavamanos-hombres"
+        },
+        {
+          "yaw": -1.7628638525510816,
+          "pitch": -0.06167220488470093,
+          "rotation": 0.7853981633974483,
+          "target": "5-34vestidores-hombres"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-33-lavamanos-hombres",
+      "name": "3.3 Lavamanos Hombres",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1422816615853328,
+          "pitch": -0.03227397321776415,
+          "rotation": 4.71238898038469,
+          "target": "5-34vestidores-hombres"
+        },
+        {
+          "yaw": -0.658634518115214,
+          "pitch": -0.004851602243949316,
+          "rotation": 1.5707963267948966,
+          "target": "3-32-espejos-hombres"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-34vestidores-hombres",
+      "name": "3.4Vestidores Hombres",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.054627111551479,
+          "pitch": 0.10981201996972523,
+          "rotation": 1.5707963267948966,
+          "target": "4-33-lavamanos-hombres"
+        },
+        {
+          "yaw": -2.7167302115187617,
+          "pitch": 0.13637171895666178,
+          "rotation": 5.497787143782138,
+          "target": "3-32-espejos-hombres"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-35-baos-hombres",
+      "name": "3.5 Baños Hombres",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1088386623434427,
+          "pitch": 0.15906149523051383,
+          "rotation": 0,
+          "target": "3-32-espejos-hombres"
+        },
+        {
+          "yaw": 2.602723236913647,
+          "pitch": 0.13286328306210216,
+          "rotation": 1.5707963267948966,
+          "target": "4-33-lavamanos-hombres"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-36-duchas-hombres",
+      "name": "3.6 Duchas Hombres",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3309605291090962,
+          "pitch": 0.09671528041095101,
+          "rotation": 0,
+          "target": "0-1-entrada"
+        },
+        {
+          "yaw": -1.739170330732506,
+          "pitch": 0.16589258132554363,
+          "rotation": 0.7853981633974483,
+          "target": "8-37-ducha-libre-hombres"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-37-ducha-libre-hombres",
+      "name": "3.7 Ducha Libre Hombres",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.057089016438746,
+          "pitch": 0.19874601408540649,
+          "rotation": 0,
+          "target": "7-36-duchas-hombres"
+        },
+        {
+          "yaw": 1.7057656430082453,
+          "pitch": 0.1288099854714968,
+          "rotation": 0,
+          "target": "15-5-patio-spa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-41-entrada-mujeres",
+      "name": "4.1 Entrada Mujeres",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0570513491608295,
+          "pitch": 0.025046473294560556,
+          "rotation": 0,
+          "target": "1-2-resepcin"
+        },
+        {
+          "yaw": 0.05969071405274562,
+          "pitch": 0.04789216077898395,
+          "rotation": 0.7853981633974483,
+          "target": "10-42-lavamanos_vestidores-mujeres"
+        },
+        {
+          "yaw": -0.21538340057248995,
+          "pitch": -0.3360419544776434,
+          "rotation": 10.995574287564278,
+          "target": "12-44-entrada-duchas-mujeress"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-42-lavamanos_vestidores-mujeres",
+      "name": "4.2 Lavamanos_vestidores Mujeres",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8851670225619213,
+          "pitch": 0.11469084556460984,
+          "rotation": 1.5707963267948966,
+          "target": "11-43-baos-mujeres"
+        },
+        {
+          "yaw": 2.8101881646450764,
+          "pitch": -0.019452507805098662,
+          "rotation": 7.0685834705770345,
+          "target": "12-44-entrada-duchas-mujeress"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-43-baos-mujeres",
+      "name": "4.3 Baños Mujeres",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2030297867779964,
+          "pitch": 0.14888729243373078,
+          "rotation": 4.71238898038469,
+          "target": "10-42-lavamanos_vestidores-mujeres"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-44-entrada-duchas-mujeress",
+      "name": "4.4 Entrada Duchas Mujeress",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04070482242479834,
+          "pitch": 0.12099740766664269,
+          "rotation": 0,
+          "target": "14-46-ducha-libre-mujeres"
+        },
+        {
+          "yaw": 0.2916645798368389,
+          "pitch": 0.19167705306859872,
+          "rotation": 0.7853981633974483,
+          "target": "13-45-baera-mujeres"
+        },
+        {
+          "yaw": -2.166974765558521,
+          "pitch": 0.193767921315807,
+          "rotation": 0.7853981633974483,
+          "target": "9-41-entrada-mujeres"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-45-baera-mujeres",
+      "name": "4.5 Bañera Mujeres",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0450979209584652,
+          "pitch": 0.25772566226448745,
+          "rotation": 0.7853981633974483,
+          "target": "14-46-ducha-libre-mujeres"
+        },
+        {
+          "yaw": -0.8914711022102608,
+          "pitch": 0.09969299967009349,
+          "rotation": 0.7853981633974483,
+          "target": "12-44-entrada-duchas-mujeress"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-46-ducha-libre-mujeres",
+      "name": "4.6 Ducha Libre Mujeres",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6106667591982973,
+          "pitch": 0.07647198640448849,
+          "rotation": 0,
+          "target": "15-5-patio-spa"
+        },
+        {
+          "yaw": 3.1153366183886515,
+          "pitch": 0.20019097499957716,
+          "rotation": 4.71238898038469,
+          "target": "13-45-baera-mujeres"
+        },
+        {
+          "yaw": -2.9506331332386218,
+          "pitch": 0.0752560309247734,
+          "rotation": 0.7853981633974483,
+          "target": "12-44-entrada-duchas-mujeress"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-5-patio-spa",
+      "name": "5 Patio Spa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.644208279534535,
+          "pitch": 0.08133011476767216,
+          "rotation": 0.7853981633974483,
+          "target": "8-37-ducha-libre-hombres"
+        },
+        {
+          "yaw": 1.6320856526180343,
+          "pitch": 0.0837091115608608,
+          "rotation": 0,
+          "target": "14-46-ducha-libre-mujeres"
+        },
+        {
+          "yaw": -0.09842344067241449,
+          "pitch": -0.05901255854510268,
+          "rotation": 0,
+          "target": "16-6-jardn-spa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-6-jardn-spa",
+      "name": "6 Jardín Spa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.06805061472193152,
+          "pitch": 0.07128745016111537,
+          "rotation": 3.141592653589793,
+          "target": "15-5-patio-spa"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
